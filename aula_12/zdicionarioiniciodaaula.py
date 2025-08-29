21160e3f34e2b770b8dd9fc3420c6f20ee09c5c1
Joao <joaogoestavares@gmail.com>
2025-08-28 21:38:59 -0300

    Create zdicionarioiniciodaaula.py

---
+++ aula_12/zdicionarioiniciodaaula.py
@@ -0,0 +1,25 @@
+##
+# Aula 12 - 28/08/2025
+# Dicionário parte 2
+# Papagaio
+# RETOMAR DEPOIS
+##
+
+##
+# Operações Básicas
+##
+
+pessoa = {"nome" : "João", "idade" : 18 , "cidade" : "Petrópolis"}
+
+#adicionar email
+pessoa["email"] = "joao@gmail.com"
+
+print(pessoa)
+
+#trocar a idade para 31
+pessoa["idade"] = 31
+
+print(pessoa)
+
+#delete a cidade
+del["cidade"]
